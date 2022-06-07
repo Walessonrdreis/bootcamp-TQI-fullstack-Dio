2e5e917ce35dfe383f57104c5f6e10b70f04637c
Walessonrdreis <wallllase@hotmail.com>
2022-06-06 21:51:09 -0300

    Conteudo  Funções javascript, até o que é this

---
+++ javascript/documentacaoJavaScript.md
@@ -0,0 +1,53 @@
+# Documentacao JS
+___
+Links Úteis
+
+
+<div 
+style=
+" width:100%; height:auto; 
+border:2px solid #559ffb;
+ font-size:16px;
+ color: #fff;
+">
+<a href="https://www.w3schools.com/js/js_arrays.asp)">Arrays</a>
+<hr/>
+
+
+
+</div>
+
+
+## lembrete 
+### Atribuição
+camel case = camelCase
+
+
+1. <strong>`return` </strong> Para a função e retorna o valor
+
+2. variáveis `const` serão sempre declaras: NOME_DA_VARIAVEL
+
+3. `calbacks` função passada como argumento para outras
+
+4. função autoincocavel.`IIFE` função anonima entre parenteses seguida por outro par de parentes
+5. Objeto `arguments` um array com todos os parâmetros passados quando a função foi invocada.
+
+6. Técnica `Spread` uma forma de lidar separadamente com elementos, usando ... Que determinam os elementos separadamente, por exemplo em um array com três elementos console.log(nomeDaFuncao (...nomeDoArray));
+7. Técnica `Rest` combina os argumentos em um array.
+8. ` Object Destructuring` Desestruturação de objeto. Entre chaves {}, podemos filtrar apenas os dados que nos interessam em um objeto.
+9. ` If else` se senão.
+10. `Switch/case` equivale a uma comparação de tipo e valor (===).  -Sempre precisa de um valor "default". -Ideal para quando se precisa comparar muitos valores.
+___
+
+11. `arrays`
+12. `FOR`
+13. `fOR...IN` Loop entre propriedades enumeradas de um objeto.
+14. `prop` equivale a cada propriedade dentro do objeto escolhido ex: for(prop in obj)...
+15. `FOR...OF` Loop entre estruturas iteráveis (arrays, strings)
+16. `WHILE`(enquanto)Executa a função até que a condição se torne falsa.
+17. `DO...WHile` Executa instruções até que a condição se torne falsa. Porém a primeira execução sempre ocorre.
+18. `THIS` Palavra reservada que é uma referencia de contexto. ex: em um objeto pessoa, this refere-se ao objeto pessoa. Ele  
+19. Sempre que um função está dentro de um objeto ela é chamada de método. quando se utiliza this. dentro de um método, ele vai se referir ao elemento pai desse método
+20.   
+
+
---
+++ javascript/funcoes/main.js
@@ -0,0 +1,472 @@
+//função autoinvocável 
+//IIFE Uma função 
+/*
+ #######
+  ##   #
+  ## #    ##  ##   #####     ####     ####     ####     #####
+  ####    ##  ##   ##  ##   ##  ##   ##  ##   ##  ##   ##
+  ## #    ##  ##   ##  ##   ##       ##  ##   ######    #####
+  ##      ##  ##   ##  ##   ##  ##   ##  ##   ##            ##
+ ####      ######  ##  ##    ####     ####     #####   ######
+
+
+*/
+
+
+
+
+/*
+//Callbacks
+/*
+   ####     ##     ####     ####     ######     ##       ####   ###  ##   #####
+  ##  ##   ####     ##       ##       ##  ##   ####     ##  ##   ##  ##  ##   ##
+ ##       ##  ##    ##       ##       ##  ##  ##  ##   ##        ## ##   #
+ ##       ##  ##    ##       ##       #####   ##  ##   ##        ####     #####
+ ##       ######    ##   #   ##   #   ##  ##  ######   ##        ## ##        ##
+  ##  ##  ##  ##    ##  ##   ##  ##   ##  ##  ##  ##    ##  ##   ##  ##  ##   ##
+   ####   ##  ##   #######  #######  ######   ##  ##     ####   ###  ##   #####
+
+
+*/
+/*
+const calc = function(operacao,num1,num2){
+  return operacao(num1,num2);
+}
+const soma=function(num1,num2){
+  return num1+num2;
+}
+const sub=function(num1,num2){
+  return num1 num2;
+}
+const resultSoma = calc(soma,1,2);
+const resultSub = calc(sub,1,2);
+console.log(resultSub);// -1
+console.log(resultSoma);//3
+
+// uma função passada para outra
+function exponencial(array,num=1){
+      const result=[];
+     for(let i=0;i<array.length;i++){
+        result.push(array[i]** num);
+     }
+      return result;
+    }
+    exponencial([1,2,3,4])
+    //[1,2,3,4]
+    exponencial([1,2,3,4],4)
+    //[1,8,27,64]
+
+
+    //encontrar o maior número enviado nos argumentos da função
+    //fazendo um loop entre todos os arguments
+    function findMax(){
+      let max = -Infinity;
+     for(let i=0; i <arguments.length; i++){
+        if(arguments[i]>max){
+          max=arguments[i];
+       }
+     }
+                  
+      return max;
+   }   
+   
+   //Spread
+   /*
+  #####                                          ###
+ ##   ##                                          ##
+ #        ######   ######    ####     ####        ##
+  #####    ##  ##   ##  ##  ##  ##       ##    #####
+      ##   ##  ##   ##      ######    #####   ##  ##
+ ##   ##   #####    ##      ##       ##  ##   ##  ##
+  #####    ##      ####      #####    #####    ######
+          ####
+
+   */
+  /*
+   function sum(x,y,z){
+  return x + y + Z;
+}
+const numbers=[1,2,3];
+console.log(sum(... numbers));
+
+//Rest
+/*
+ ######   #######   #####   ######
+  ##  ##   ##   #  ##   ##  # ## #
+  ##  ##   ## #    #          ##
+  #####    ####     #####     ##
+  ## ##    ## #         ##    ##
+  ##  ##   ##   #  ##   ##    ##
+ #### ##  #######   #####    ####
+
+
+*/
+/*
+function confereTamanho(... args){
+ console.log(args.length);
+}
+confereTamanho()//0
+confereTamanho(1,2)//2
+confereTamanho(3,4,5)//3
+
+//object Destructuring 
+const user={
+  td:42,
+ displayName:'jdoe',
+  fullName:{
+    firstName:'John',
+    lastName:'Doe'
+};
+function userId({id}){
+  return id;
+}
+function getFullName({fullName:(firstName:first,lastName:last}}){
+  return $(first)$(last)';
+userId(user)
+// 42
+getFullName(user)
+// John Doe
+ }
+
+ // if/else
+
+ /*
+  ####    #######                    #######  ####      #####   #######
+   ##      ##   #                     ##   #   ##      ##   ##   ##   #
+   ##      ## #                       ## #     ##      #         ## #
+   ##      ####                       ####     ##       #####    ####
+   ##      ## #                       ## #     ##   #       ##   ## #
+   ##      ##                         ##   #   ##  ##  ##   ##   ##   #
+  ####    ####                       #######  #######   #####   ######
+ */
+/*
+ function numeroPositivo(num){
+  let resultado;
+  if(num<0){
+    resultado=false;
+  }else{
+    resultado=true;
+ }
+  return resultado;
+}
+numeroPositivo(2)
+// true
+numeroPositivo(-9)
+// false
+
+////
+function numeroPositivo(num){
+ let resultado;
+  if(num<0){
+    resultado=false;
+ }else{
+    resultado=true;
+ }
+  return resultado;
+}
+numeroPositivo(2)
+// true
+numeroPositivo(-9)
+// false
+////boas práticas
+function numeroPositivo(num){
+  let resultado;
+  const ehNegativo=num<0;
+  if(ehNegativo){
+    resultado=false;
+  }else{
+    resultado=true;
+ }
+  return resultado;
+}
+///
+function numeroPositivo(num){
+ const ehNegativo=num<0;
+ if(ehNegativo){
+    return false;
+ }
+  return true;
+}
+/// if/else aninhados
+
+
+function numeroPositivo(num){
+const ehNegativo=num<0;
+const maiorQueDez=num>10;>
+
+if(ehNegativo){
+return "Esse número é negativo!";
+}else if(!ehNegativo && maiorQueDez){
+return "Esse número é positivo e maior que 10!"
+}
+return "Esse número é positivo!";
+}
+
+///
+
+/*        
+
+  #####   ##   ##   ####    ######     ####   ##   ##
+ ##   ##  ##   ##    ##     # ## #    ##  ##  ##   ##
+ #        ##   ##    ##       ##     ##       ##   ##
+  #####   ## # ##    ##       ##     ##       #######
+      ##  #######    ##       ##     ##       ##   ##
+ ##   ##  ### ###    ##       ##      ##  ##  ##   ##
+  #####   ##   ##   ####     ####      ####   ##   ##
+
+                     ####     ##      #####   #######
+      ##            ##  ##   ####    ##   ##   ##   #
+     ##            ##       ##  ##   #         ## #
+    ##             ##       ##  ##    #####    ####
+   ##              ##       ######        ##   ## #
+  ##                ##  ##  ##  ##   ##   ##   ##   #
+ ##                  ####   ##  ##    #####   #######
+
+
+*/
+
+/*
+
+   function getAnimal(id){
+  switch(id){
+    case1:
+      return"cão";
+    case2:
+      return"gato";
+    case3:
+      return"pássaro";
+    default:
+      return"peixe";
+}
+getAnimal(1)// cão
+getAnimal(4)// peixe
+getAnimal("1")// peixe / entende que é algo diferente por isso retorna o valor default 
+ }
+ ///
+ //FOR
+/*
+ #######   #####   ######
+  ##   #  ##   ##   ##  ##
+  ## #    ##   ##   ##  ##
+  ####    ##   ##   #####
+  ## #    ##   ##   ## ##
+  ##      ##   ##   ##  ##
+ ####      #####   #### ##
+
+
+*/
+/*
+ function multiplicaPorDois(arr){
+  let multiplicados=[];
+  for(let i=0;i<arr.length;i++){
+   multiplicados.push(arr[i]*2);
+ }
+  return multiplicados;
+}
+const meusNumeros=[2,33,456,356,40];
+multiplicaPorDois(meusNumeros);
+//[4,66,912,712,80]
+
+///
+
+
+   
+   */
+    /*
+#######                                                ####
+  ##   #                                                 ##
+  ## #     ####    ######                                ##     #####
+  ####    ##  ##    ##  ##                               ##     ##  ##
+  ## #    ##  ##    ##                                   ##     ##  ##
+  ##      ##  ##    ##        ##       ##       ##       ##     ##  ##
+ ####      ####    ####       ##       ##       ##      ####    ##  ##
+
+
+    /*
+    InExemplo(obj){function for
+  for(prop in obj){
+  console.log(prop);
+  }
+  }
+  const meuObjeto={
+  nome:"João",
+  idade:"20",
+  cidade:"Salvador"
+  }
+  for InExemplo(meuObjeto);
+  // nome
+  // idade
+  // cidadE
+    */
+   /*
+  InExemplo(obj){function for
+    for(prop in obj){
+    console.log(obj[prop]);
+    }
+    }
+    const meuObjeto={
+    nome:"João",
+    idade:"20",
+    cidade:"Salvador"
+    }
+    for InExemplo(meuObjeto);
+    // João
+    V/20
+    // Salvador
+   /*
+   /*
+
+ #######                                                #####     ###
+  ##   #                                               ##   ##   ## ##
+  ## #     ####    ######                              ##   ##    #
+  ####    ##  ##    ##  ##                             ##   ##  ####
+  ## #    ##  ##    ##                                 ##   ##   ##
+  ##      ##  ##    ##        ##       ##       ##     ##   ##   ##
+ ####      ####    ####       ##       ##       ##      #####   ####
+
+ function logLetras(palavra){
+   for(letra of palavra){
+     console.log(letra);
+   }
+ }
+
+ const palavra = "abacaxi";
+ loLetras(palavra)
+ //a
+ //b 
+ //a 
+ //c 
+ //a
+ //x
+ //i
+///////
+function logNumeros(nuns){
+  for(num of nums){
+    console.log(num;)
+  }
+}
+
+const nums = [30, 20, 233, 2];
+
+logLetras(nums)
+//30
+//20
+//233
+//2
+
+    */
+   /*
+ ##   ##  ###        ##      ###
+ ##   ##   ##                 ##
+ ##   ##   ##       ###       ##      ####
+ ## # ##   #####     ##       ##     ##  ##
+ #######   ##  ##    ##       ##     ######
+ ### ###   ##  ##    ##       ##     ##
+ ##   ##  ###  ##   ####     ####     #####
+
+
+  funtion exemploWhile(){
+    let num = 0
+
+    while(num <= 5){
+      console.log(num);
+      num++;
+    }
+  }
+  exempleWhile()
+  //0
+  //1
+  //2
+  //3
+  //4
+  //5
+  
+   */
+  /*
+ #####                                        ##   ##  ###        ##      ###
+  ## ##                                       ##   ##   ##                 ##
+  ##  ##   ####                               ##   ##   ##       ###       ##      ####
+  ##  ##  ##  ##                              ## # ##   #####     ##       ##     ##  ##
+  ##  ##  ##  ##                              #######   ##  ##    ##       ##     ######
+  ## ##   ##  ##     ##       ##       ##     ### ###   ##  ##    ##       ##     ##
+ #####     ####      ##       ##       ##     ##   ##  ###  ##   ####     ####     #####
+
+ function exempleDoWhile(){
+   let num = 0;
+
+   do{
+     console.log(num);
+     num++
+   }while(num <= 5)
+ }
+
+ exempleDoWhile()
+ //0
+ //1
+ //2
+ //3
+ //4
+ //5
+
+ function exempleDoWhile(){
+   let num = 6;
+
+   do {
+     console.log(num);
+     num++
+   }while(num <= 5)
+ }
+ 
+ exempleDoWhile()
+ //6 
+
+  */
+ /*
+ ######   ###        ##
+ # ## #    ##
+   ##      ##       ###      #####
+   ##      #####     ##     ##
+   ##      ##  ##    ##      #####
+   ##      ##  ##    ##          ##
+  ####    ###  ##   ####    ######
+
+ const pessoa = {
+   firstName: "André",
+   lastName: "Soares",
+   id      : 1,
+   fullName: function(){
+     return this.firstName + " " + this.lastName;
+   },
+   getId: function(){
+     return this.id;
+   }
+ };
+
+ pessoa.fullName();
+ //"André Soares"
+ pessoa.getId();
+ //1
+
+ ///
+ const pessoa = {
+   firstName: 'Diego',
+   lastName: 'Vieira',
+  getFullName: function(){
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+ };
+
+ getFullName();
+ */
+
+
+function findMax() {
+    let max = -Infinity;//-infinity para garantir que possa ser mandado to tipo de numero tanto negativo quanto positivo
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            max = arguments[i];
+        }
+    }
+    return max;
+}
+document.getElementById("demo").textContent = findMax(4, 5, 6);
