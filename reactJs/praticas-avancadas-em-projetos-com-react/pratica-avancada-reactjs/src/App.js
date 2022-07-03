531a368203fe8d083a9d75b3ad850fa4aa2671b2
Walessonrdreis <wallllase@hotmail.com>
2022-07-03 09:27:07 -0300

    aula 1.1 ciclo de vida

---
+++ reactJs/praticas-avancadas-em-projetos-com-react/pratica-avancada-reactjs/src/App.js
@@ -0,0 +1,52 @@
+import React,{Component} from 'react';
+import Twitter from './Twitter'
+
+class App extends Component{
+
+  state={ 
+    loading: false,
+    actived:true
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        loading: true
+      })
+    },3000)
+  }
+
+  onRemove = () =>{
+    this.setState({
+      actived:false
+    })
+  }
+
+  render() {
+    const posts = [{ 
+      title: 'xpto',
+      description: 'foo'
+    },{ 
+      title: 'xpto',
+      description: 'foo'
+    }]
+
+    return(
+
+      <div>
+      <button onClick={this.onRemove}>Remover Componente</button>
+      {this.state.actived && (
+      <Twitter posts={posts} />
+      )}
+      </div>
+    )
+    
+
+  }
+}
+
+
+
+
+
+export default App;
